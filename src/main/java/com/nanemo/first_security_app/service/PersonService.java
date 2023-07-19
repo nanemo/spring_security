27d02705718632b04733422942594ee42247f039
Nadir Nadirli <companynadirnadirli@gmail.com>
2023-07-20 01:04:48 +0400

    I want to create PersonService for creating and checking about a existing a User

--- src/main/java/com/nanemo/first_security_app/service/PersonService.java
+++ src/main/java/com/nanemo/first_security_app/service/PersonService.java
@@ -1,4 +1,18 @@
 package com.nanemo.first_security_app.service;
 
+import com.nanemo.first_security_app.entity.Person;
+import com.nanemo.first_security_app.repository.PeopleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class PersonService {
+    private final PeopleRepository peopleRepository;
+
+    //TODO create and connect this method with PersonValidator class.
+    public Optional<Person> findUserBy
+
 }
